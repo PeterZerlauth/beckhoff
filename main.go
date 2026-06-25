21246f6c309a6d9273fe8a9194f53a55d1af0f19
Peter Zerlauth <48495545+PeterZerlauth@users.noreply.github.com>
2026-06-25 14:18:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,16 +7,8 @@ import (
 )
 
 func main() {
-
-	// logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-	// 	Level: slog.LevelDebug,
-	// }))
-
+	
 	server := server.NewServer(25000, "Ads server")
-
-	// server.Symbol().Add("Main.bTest", []byte{1})  // true
-	// server.Symbol().Add("Main.bTest2", []byte{1}) // true
-	// server.Symbol().Add("Main.bValue", []byte{1}) // true
 
 	if err := server.Start(); err != nil {
 		log.Fatalf("Failed to start ADS server: %v", err)
